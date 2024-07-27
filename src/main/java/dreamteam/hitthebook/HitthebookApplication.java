e96d4469f8e37f0975fa2da0b1a4ff57a10d99ed
대선 <iuholic83@naver.com>
2024-07-28 00:24:29 +0900

    First commit

---
+++ src/main/java/dreamteam/hitthebook/HitthebookApplication.java
@@ -0,0 +1,13 @@
+package dreamteam.hitthebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HitthebookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HitthebookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dreamteam/hitthebook/HitthebookApplicationTests.java
@@ -0,0 +1,13 @@
+package dreamteam.hitthebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HitthebookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
