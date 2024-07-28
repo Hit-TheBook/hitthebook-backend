27a5ee6a1da69ae658253712aff1b802a9763a12
대선 <iuholic83@naver.com>
2024-07-28 19:00:39 +0900

    Feat : 엔티티 설계중

---
+++ src/main/java/dreamteam/hitthebook/domain/plannerschedule/entity/PlannerSchedule.java
@@ -0,0 +1,46 @@
+package dreamteam.hitthebook.domain.plannerschedule.entity;
+
+import dreamteam.hitthebook.common.entity.BaseEntity;
+import dreamteam.hitthebook.domain.member.entity.Member;
+import dreamteam.hitthebook.domain.plannerschedule.enumulation.ScheduleTypeEnum;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Where(clause = "is_deleted = false")
+@SQLDelete(sql = "UPDATE planner_schedule SET is_deleted = true WHERE schedule_id = ?")
+@Getter @Setter
+@Table(name = "planner_schedule")
+public class PlannerSchedule extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "planner_schedule_id")
+    private Long plannerScheduleId;
+
+    private LocalDateTime startedAt;
+
+    private LocalDateTime finishedAt;
+
+//    @Column(name = "schedule_title", nullable = false, length = 50)
+    private String scheduleTitle;
+
+//    @Column(name = "schedule_content", length = 300)
+    private String scheduleContent;
+
+    private ScheduleTypeEnum scheduleType;
+
+    private Boolean alarmRequired; // ~분전 알림을 주는 기획으로 추가
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+}
---
+++ src/main/java/dreamteam/hitthebook/domain/plannerschedule/enumulation/ScheduleTypeEnum.java
@@ -0,0 +1,10 @@
+package dreamteam.hitthebook.domain.plannerschedule.enumulation;
+
+public enum ScheduleTypeEnum {
+    ROUTINE,
+    IMPORTANT,
+    TEST,
+    EVENT,
+    GAME,
+    APPOINTMENT // 만남의 약속
+}
