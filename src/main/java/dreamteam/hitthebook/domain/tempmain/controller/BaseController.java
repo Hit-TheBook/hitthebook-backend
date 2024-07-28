1c0815b475de94a0c93aa2e8608180d41ea391cb
대선 <iuholic83@naver.com>
2024-07-28 20:18:22 +0900

    Feat : 엔티티 설계중

---
+++ src/main/java/dreamteam/hitthebook/domain/tempmain/controller/BaseController.java
@@ -0,0 +1,18 @@
+package dreamteam.hitthebook.domain.tempmain.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/")
+public class BaseController {
+    @GetMapping
+    public String home() {
+        return "Hello World!!!!!!!!!!!!!!!!!!!!!";
+    }
+}
---
+++ src/main/java/dreamteam/hitthebook/domain/timer/entity/Timer.java
@@ -0,0 +1,39 @@
+package dreamteam.hitthebook.domain.timer.entity;
+
+import dreamteam.hitthebook.common.entity.BaseEntity;
+import dreamteam.hitthebook.domain.member.entity.Member;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Where(clause = "is_deleted = false")
+@SQLDelete(sql = "UPDATE timer SET is_deleted = true WHERE timer_id = ?")
+@Getter @Setter
+public class Timer extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "timer_id", nullable = false)
+    private Long timerId;
+
+//    @Column(name = "subject_name", length = 20)
+    private String subjectName;
+
+    @Column(name = "study_finish_time")
+    private LocalDateTime studyFinishTime;
+
+    @Column(name = "study_time_length")
+    private Integer studyTimeLength;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+}
