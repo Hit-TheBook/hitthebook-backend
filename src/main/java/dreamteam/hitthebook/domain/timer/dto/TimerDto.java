c68f9c0a7db6fdcbede270308342b218862d093d
Hanseung2 <dgs6400@naver.com>
2024-08-08 23:33:09 +0900

    타이머 기능 수정

--- src/main/java/dreamteam/hitthebook/domain/timer/dto/TimerDto.java
+++ src/main/java/dreamteam/hitthebook/domain/timer/dto/TimerDto.java
@@ -1,26 +1,69 @@
 package dreamteam.hitthebook.domain.timer.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import dreamteam.hitthebook.domain.timer.entity.Timer;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.Duration;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TimerDto {
 
     @Data
     @NoArgsConstructor
-    public static class TimerRequestDto{
+    public static class TimerStartRequestDto{
 
         @Schema(description = "과목명", example = "수학", type = "string")
         private String subjectName; //과목명
+
+    }
+
+    @Data
+    @NoArgsConstructor
+    public static class TimerEndRequestDto {
 
         @Schema(description = "타이머 종료 시간", example = "00:00:00", type = "string")
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm:ss", timezone = "UTC")
         private Duration studyTimeLength;
 
+    }
+
+    @Data
+    @NoArgsConstructor
+    public static class TimerDateDto{
+
+        @Schema(description = "날짜 입력", example = "2024-08-08", type = "string")
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "UTC")
+        private LocalDate studyDate;
+
+    }
+
+    @Data
+    @NoArgsConstructor
+    public static class TimerListDto{
+
+        private List<TimerContents> timerContentsList = new ArrayList<>();
+        public TimerListDto(List<Timer> timerList) {
+            for(Timer timer : timerList) {
+                this.timerContentsList.add(new TimerContents(timer));
+            }
+        }
+    }
+
+    @Data
+    @NoArgsConstructor
+    public static class TimerContents{
+
+        private String subjectName;
+        private Duration studyTimeLength;
+
+        public TimerContents(Timer timer){
+            this.subjectName=timer.getSubjectName();
+            this.studyTimeLength=timer.getStudyTimeLength();
+        }
     }
 }
--- src/main/java/dreamteam/hitthebook/domain/timer/repository/TimerRepository.java
+++ src/main/java/dreamteam/hitthebook/domain/timer/repository/TimerRepository.java
@@ -1,10 +1,19 @@
 package dreamteam.hitthebook.domain.timer.repository;
 
+import dreamteam.hitthebook.domain.member.entity.Member;
 import dreamteam.hitthebook.domain.timer.entity.Timer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;;
+import java.util.List;
 
 @Repository
 public interface TimerRepository extends JpaRepository<Timer, Long> {
+
+    @Query("SELECT t FROM Timer t WHERE t.member = :member AND DATE(t.updatedAt) = :studyDate")
+    List<Timer> findByMemberAndUpdatedAt(@Param("member")Member member, @Param("studyDate")LocalDate studyDate);
 
 }
